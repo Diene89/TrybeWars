bc6e7873f7502c02d053bead03ecca57404fa3ae
Diene <email.da.diene@gmail.com>
2022-04-25 13:00:51 -0300

    Requisito 3 - um dia vai

--- src/Components/Hearder.js
+++ src/Components/Hearder.js
@@ -2,13 +2,52 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function Header() {
-  const { getFilterByName } = useContext(StarWarsContext);
+  const { getFilterByName,
+    handleButtonClick, getColumn, getComparison, getValue } = useContext(StarWarsContext);
   return (
     <form>
       <input
         data-testid="name-filter"
         onChange={ getFilterByName }
       />
+
+      <select
+        data-testid="column-filter"
+        name="column"
+        onChange={ getColumn }
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation period</option>
+        <option value="surface_water">surface water</option>
+      </select>
+
+      <select
+        data-testid="comparison-filter"
+        name="comparison"
+        onChange={ getComparison }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+
+      <input
+        data-testid="value-filter"
+        name="value"
+        type="number"
+        onChange={ getValue }
+      />
+
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleButtonClick }
+      >
+        Filtrar
+      </button>
+
     </form>
   );
 }
--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -6,6 +6,12 @@ import planetAPI from '../Service/PlanetAPI';
 function StarWarsProvider({ children }) {
   const [data, setData] = useState();
   const [filter, setFilter] = useState({ filterByName: { name: '' } });
+  const [filterByNumericValues, setFilterByNumericValues] = useState(
+    [],
+  );
+  const [filterColumn, setFilterColumn] = useState('population');
+  const [filterComparison, setFilterComparison] = useState('maior que');
+  const [filterValue, setFilterValue] = useState(0);
 
   async function getPlanet() {
     const planetResponse = await planetAPI();
@@ -16,8 +22,58 @@ function StarWarsProvider({ children }) {
     getPlanet();
   }, []);
 
+  // Requisito 2
   const getFilterByName = ({ target }) => {
     setFilter({ filterByName: { name: target.value } });
+  };
+
+  const getColumn = ({ target }) => {
+    setFilterColumn(target.value);
+  };
+
+  const getComparison = ({ target }) => {
+    setFilterComparison(target.value);
+  };
+
+  const getValue = ({ target }) => {
+    setFilterValue(target.value);
+  };
+
+  // Requisito 3
+  const getFilterByOption = () => {
+    setFilterByNumericValues([...filterByNumericValues,
+      { column: filterColumn,
+        comparison: filterComparison,
+        value: filterValue }]);
+  };
+
+  const handleButtonClick = () => {
+    // console.log(filterByNumericValues);
+    getFilterByOption();
+    const { comparison } = filterComparison;
+    const { column } = filterColumn;
+    const { value } = filterValue;
+
+    if (comparison === 'maior que') {
+      return column > value;
+    }
+    if (comparison === 'menor que') {
+      return column < value;
+    }
+    if (comparison === 'igual a') {
+      return column === value;
+    }
+
+    // switch (comparison) {
+    // case 'maior que':
+    //   return setData(data.filter((element) => element[column] > value));
+    // case 'menor que':
+    //   return setData(data.filter((element) => element[column] < value));
+    // case 'igual a':
+    //   return setData(data.filter((element) => element[column] === value));
+    // default:
+    //   break;
+    // }
   };
 
   const contextValue = {
@@ -25,6 +81,15 @@ function StarWarsProvider({ children }) {
     getPlanet,
     getFilterByName,
     filter,
+    getFilterByOption,
+    handleButtonClick,
+    getColumn,
+    getComparison,
+    getValue,
+    filterColumn,
+    filterComparison,
+    filterValue,
+    filterByNumericValues,
   };
   return (
     <StarWarsContext.Provider value={ contextValue }>
